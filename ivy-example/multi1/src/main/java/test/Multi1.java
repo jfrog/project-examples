c3a0042d2ea002d96c2b201da5772fb1e20a2cbf
Fred Simon <freds@jfrog.com>
2011-09-18 12:08:13 +0300

    First project samples in Git

---
+++ ivy-example/multi1/src/main/java/test/Multi1.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Hello world!
+ */
+public class Multi1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ivy-example/multi2/src/main/java/test/App.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
