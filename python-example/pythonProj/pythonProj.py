2c3ffc82a7b26be99abf2b6b4f44ba2396ea41ee
Michal Reuven <michalr@jfrog.com>
2014-10-29 10:35:18 -0700

    fix

--- python-example/pythonProj/pythonProj.py
+++ python-example/pythonProj/pythonProj.py
@@ -2,9 +2,9 @@
 
 # Function definition is here
 def printme( str ):
-    "This prints a passed string into this function"
-     print str;
-     return;
+    # This prints a passed string into this function
+    print (str)
+    return;
 
 # Now you can call printme function
 printme("Hello from JFROG");
