8e0019aef83dcd21bca12ae70fbd6c5568282037
eyalbe4 <eyalbenmoshe@jfrog.com>
2015-04-29 18:24:49 +0300

    Move all gradle-exaples to a new folder

---
+++ gradle-examples/gradle-jcenter-resolve/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Hello world!
+ */
+import org.apache.commons.lang.StringUtils;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("hello world"));
+    }
+}
