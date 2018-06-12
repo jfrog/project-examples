4fe7c99fff48e662a2ea8051a19dc735130b3bfa
Elio Marcolino <eliom@jfrog.com>
2018-06-11 17:17:47 -0700

    Added custom image docker file

--- node-version/reset_demo.sh
+++ node-version/reset_demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git pull origin master
-sed -i_old 's/FROM.*/FROM node:6/' Dockerfile
+sed -i_old 's/FROM.*/FROM node:6.14.2-stretch/' Dockerfile
 git commit Dockerfile -m "Reset demo"
 git push origin master
