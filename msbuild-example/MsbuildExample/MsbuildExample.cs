7eec4f5ea4c80537a46a323f600841cae695d633
Eyal Ben Moshe <eyalb@jfrog.com>
2020-07-09 01:13:33 +0300

    Simplify msbuild-example

--- msbuild-example/MsbuildExample/MsbuildExample.cs
+++ msbuild-example/MsbuildExample/MsbuildExample.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MsbuildLibrary
+namespace MsbuildExample
 {
-    public class LibraryExample
+    public class MsbuildExample
     {
         public static string HelloWorld()
         {
