fff0ceceb7a96c747df7319c768fde4f17540241
Tal Yitzhak <talyitzhak100@gmail.com>
2020-07-26 18:05:45 +0300

    added feature branch repo managment with the jfrog cli example - blog… (#208)
    * added feature branch repo managment with the jfrog cli example - blog post by tal yitzhak
    
    * corrected eyal's CR suggestions
    
    * moved repo management github actions example to the dedicated folder or github examples

---
+++ github-action-examples/repo-management-github-actions-example/README.md
@@ -0,0 +1,4 @@
+# Repo Management GitHub Actions Example
+
+This example demonstrates how you can use GitHub Actions to automate the creation and deletion of Artifactory repositories, which correspond to feature branches that are created on GitHub.
+The full description of how to set up and run this example are available on <---this blog post--->.
---
+++ repo-management-github-actions-example/maths/src/main/java/com/dineshvelhal/javaprojects/maths/Math.java
@@ -0,0 +1,28 @@
+package com.dineshvelhal.javaprojects.maths;
+   
+
+/**
+ * @author Dinesh_Velhal (dinesh.velhal at gmail)
+ * 
+ * A dummy Math class used for testing the Jenkins pipeline builds.
+ * 
+ *
+ */
+public class Math 
+{
+    public int add(int a, int b) {
+    	return a + b + 0 + 0 + 0;
+    }
+    
+    public int subtract(int a, int b) {
+    	return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+    	return a * b;
+    }
+    
+    public int divide(int a, int b) {
+    	return a / b;
+    }
+}
