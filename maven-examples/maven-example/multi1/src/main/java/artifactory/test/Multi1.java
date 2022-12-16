bf1ceb72f2d247fad1002dd9ca952a23e1d81ee1
Jonathan Wehner <jonathanw@jfrog.com>
2022-12-16 16:28:31 -0500

    triggering build from commit

--- maven-examples/maven-example/multi1/src/main/java/artifactory/test/Multi1.java
+++ maven-examples/maven-example/multi1/src/main/java/artifactory/test/Multi1.java
@@ -5,6 +5,6 @@ package artifactory.test;
  */
 public class Multi1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Artifactory!");
     }
 }
